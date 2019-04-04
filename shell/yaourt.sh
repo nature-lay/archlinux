454f5a4b5639107614971c5c63440788cf664698
lay <lay771880416@163.com>
2019-04-04 09:25:04 +0800

    2019-4-4

--- shell/yaourt.sh
+++ shell/yaourt.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 # 安装vim
-pacman -S vim
+pacman -S vim reflector
 
-repo[0]='Server = http://mirrors.163.com/archlinux/$repo/os/$arch'
-repo[1]='Server = http://mirror.bjtu.edu.cn/archlinux/$repo/os/$arch'
-repo[2]='Server = http://mirror6.bjtu.edu.cn/archlinux/$repo/os/$arch'
-repo[3]='Server = ftp://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'
-repo[4]='Server = http://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'
-repo[5]='Server = ftp://mirror.lzu.edu.cn/archlinux/$repo/os/$arch'
-repo[6]='Server = http://mirror.lzu.edu.cn/archlinux/$repo/os/$arch'
-repo[7]='Server = ftp://mirrors.stuhome.net/archlinux/$repo/os/$arch'
-repo[8]='Server = http://mirrors.stuhome.net/archlinux/$repo/os/$arch'
-
-sed -i "8d" /etc/pacman.d/mirrorlist
-for item in ${repo[@]}
-do
-  sed -i "8i$item" /etc/pacman.d/mirrorlist
-done
+cp /etc/pacman.d/mirrorlist  /etc/pacman.d/mirrorlist.backup
+# 按速度重排
+reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist

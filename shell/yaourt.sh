9055f1d1cf9914a1adf0e6747e2b245463588ec4
lay <lay771880416@163.com>
2019-04-04 09:09:57 +0800

    2019-4-4

--- shell/yaourt.sh
+++ shell/yaourt.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # 安装vim
 pacman -S vim
-repo = (
-  'Server = http://mirrors.163.com/archlinux/$repo/os/$arch'
-  'Server = http://mirror.bjtu.edu.cn/archlinux/$repo/os/$arch'
-  'Server = http://mirror6.bjtu.edu.cn/archlinux/$repo/os/$arch'
-  'Server = ftp://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'
-  'Server = http://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'
-  'Server = ftp://mirror.lzu.edu.cn/archlinux/$repo/os/$arch'
-  'Server = http://mirror.lzu.edu.cn/archlinux/$repo/os/$arch'
-  'Server = ftp://mirrors.stuhome.net/archlinux/$repo/os/$arch'
-  'Server = http://mirrors.stuhome.net/archlinux/$repo/os/$arch'
-)
+
+repo[0]='Server = http://mirrors.163.com/archlinux/$repo/os/$arch'
+repo[1]='Server = http://mirror.bjtu.edu.cn/archlinux/$repo/os/$arch'
+repo[2]='Server = http://mirror6.bjtu.edu.cn/archlinux/$repo/os/$arch'
+repo[3]='Server = ftp://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'
+repo[4]='Server = http://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'
+repo[5]='Server = ftp://mirror.lzu.edu.cn/archlinux/$repo/os/$arch'
+repo[6]='Server = http://mirror.lzu.edu.cn/archlinux/$repo/os/$arch'
+repo[7]='Server = ftp://mirrors.stuhome.net/archlinux/$repo/os/$arch'
+repo[8]='Server = http://mirrors.stuhome.net/archlinux/$repo/os/$arch'
+
 sed -i "8d" /etc/pacman.d/mirrorlist
 for item in ${repo[@]}
 do

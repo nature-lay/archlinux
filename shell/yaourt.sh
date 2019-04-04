00b90f82af4cdaf783ba4a8bc75eceddbfcf2d68
lay <lay771880416@163.com>
2019-04-04 09:38:21 +0800

    2019-4-4

--- shell/yaourt.sh
+++ shell/yaourt.sh
@@ -5,3 +5,16 @@ pacman -S vim reflector
 cp /etc/pacman.d/mirrorlist  /etc/pacman.d/mirrorlist.backup
 # 按速度重排
 reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
+
+
+# 安装AUR
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist"
+echo "[archlinuxcn]" >> /etc/pacman.conf
+echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
+echo "Server= https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
+
+# 同步并安装yaourt
+pacman -Syu yaourt
+
+# 安装fcitx和搜狗输入法
